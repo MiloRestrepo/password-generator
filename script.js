ad26585674f82499245d745bff939186e49d1153
Troy <camilo1997@yahoo.com>
2022-01-20 17:18:00 -0500

    added functions and vars

--- script.js
+++ script.js
@@ -1,5 +1,59 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercase = ["a", "b", "c", "d"]
+var uppercase = ["A", "B"]
+var numeric = ["0", "1", "2"]
+var special = ["@", "/"]
+
+
+function getOptions() {
+  var length = parseInt(prompt("What is the number of characters you want in your password?")
+  );
+  if (Number.isNaN(length)) {
+    alert("Password length must be a number");
+    return null;
+  }
+  if (length < 8 || length > 128) {
+    alert("Password must be between 8 characters and 128");
+    return null;
+
+  }
+  var hasLowercase = confirm("Click ok to include lowercase characters");
+  var hasUppercase = confirm("Click ok to include Uppercase characters");
+  var hasNumeric = confirm("Click ok to include lowercase characters");
+  var hasLowercase = confirm("Click ok to include lowercase characters");
+
+  if (hasLowercase === false && hasUppercase === false && hasNumeric === false && hasSpecial === false) {
+    alert("You must chose atleast 1 character type");
+    return null;
+  }
+  var passwordOptions = {
+    length: length,
+    hasLowercase: lowercase,
+
+  };
+  return passwordOptions;
+
+
+
+
+}
+function randomArr(arr) {
+  var indexRandom = Math.floor(Math.random() * arr.length);
+  var randomEl = arr[indexRandom];
+  return randomEl;
+}
+
+function generatePassword() {
+
+
+
+
+}
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
