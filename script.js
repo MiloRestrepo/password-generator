9d204c668aad94afa0dd35a71dbf94ab59b62782
Troy <camilo1997@yahoo.com>
2022-01-25 15:08:22 -0500

    added for loop for options only

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 var lowercase = ["a", "b", "c", "d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercase = ["A", "B", "C", "D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ["0", "1", "2","3","4","5","6","7","8","9"];
@@ -76,6 +75,11 @@ if (options.hasNumeric){
 if (options.hasSpecial){
   possible = possible.concat(special);
   gaurantee.push(randomArr(special))
+}
+
+for(var i = 0; i < options.length; i++){
+  var possible = randomArr(possible);
+  result.push(possible);
 }
 
 for(var i = 0; i < gaurantee.length; i++){
